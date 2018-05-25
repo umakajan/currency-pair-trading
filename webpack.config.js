aebbfd375be8bb53e1e65d1fa035259af85a0b27
Umakajan Umakanthan <umakajan@umakajan.com>
2018-05-24 20:28:16 -0400

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: __dirname + '/src/index.js',
   output: {
@@ -15,4 +17,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html'
+    })
+  ],
 };
